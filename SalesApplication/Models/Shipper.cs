4def86071f373018407648247bfd74fd24ce4000
MudassiraAnjum <emudassiraanjum@gmail.com>
2024-12-02 15:34:57 +0530

    Main-With Models and Data

---
+++ SalesApplication/Models/Shipper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesApplication.Models;
+
+public partial class Shipper
+{
+    public int ShipperId { get; set; }
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string Password { get; set; } = null!;
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
