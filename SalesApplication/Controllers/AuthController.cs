5940911920faa6a5a6224f6684e52d1ac7ff382d
MudassiraAnjum <emudassiraanjum@gmail.com>
2024-12-06 10:52:22 +0530

    removed try catch block

--- SalesApplication/Controllers/AuthController.cs
+++ SalesApplication/Controllers/AuthController.cs
@@ -21,29 +21,15 @@ namespace SalesApplication.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] ShipperRegistrationDto registrationDto)
         {
-            try
-            {
                 var result = await _authService.RegisterShipperAsync(registrationDto);
                 return Ok(result);
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while registering.");
-            }
         }
 
         [HttpPost("login")]
         public IActionResult Login(string username, string password)
         {
-            try
-            {
                 var tokenModel = _authService.Authenticate(username, password);
-                return Ok(new { token = tokenModel.Token, role = tokenModel.Role });
-            }
-            catch (UnauthorizedAccessException)
-            {
-                return Unauthorized("Invalid username or password");
-            }
+            return Ok(new { token = tokenModel.Token, role = tokenModel.Role });
         }
 
     }
